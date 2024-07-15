00d62eeb02efb8316d2e7d32ca11c8577dc18129
penumala seshaeta <2100030428@kluiversity.in>
2024-07-15 12:06:50 +0530

    Fourth commit

---
+++ script.js
@@ -0,0 +1,39 @@
+//about 
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+//for small screen
+var sidemeu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
+//contact form
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw6iV0CQ7k1rgGp32S6hfLSRYl32b8n-3O3h6tne2VODoL9NNq7Pq4i1FdjIeKNyWM6Ug/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
